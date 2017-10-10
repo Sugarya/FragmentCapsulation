680f474bd630246e091b0e7e9f9c637a29c945e7
EthanRuan <sugaryaruan@gmail.com>
2017-10-10 21:36:19 +0800

    optimzie BasicToolbarActivity which adapte android 4.4 status bar

--- app/src/main/java/com/sugary/fragmentcapsulation/base/BasicToolBarActivity.java
+++ app/src/main/java/com/sugary/fragmentcapsulation/base/BasicToolBarActivity.java
@@ -1,11 +1,14 @@
 package com.sugary.fragmentcapsulation.base;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.sugary.fragmentcapsulation.R;
@@ -26,7 +29,11 @@ public abstract class BasicToolBarActivity extends BasicActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initToolbar();
+        adaptStatusBar();
+    }
 
+    private void initToolbar() {
         mToolbar.setTitle("");
         mToolbar.setSubtitle("");
         setSupportActionBar(mToolbar);
@@ -37,6 +44,25 @@ public abstract class BasicToolBarActivity extends BasicActivity {
                 onBackPressed();
             }
         });
+    }
+
+    /**
+     * Android 4.4时，设置状态栏透明，来适配标题栏样式
+     */
+    private void adaptStatusBar() {
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
+            if (mToolbar != null) {
+                WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+                localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+
+                int toolbarHeight = (int)getResources().getDimension(R.dimen.tool_bar_height_adapt);
+                ViewGroup.LayoutParams layoutParams = mToolbar.getLayoutParams();
+                layoutParams.height = toolbarHeight;
+
+                int toolbarPaddingTop = (int) getResources().getDimension(R.dimen.tool_bar_padding_top);
+                mToolbar.setPadding(0, toolbarPaddingTop, 0, 0);
+            }
+        }
     }
 
     /**
